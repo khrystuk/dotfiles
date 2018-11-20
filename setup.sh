94d4f1a17381fb4fac578ef41b7f2739d19f8249
yura khrystuk <22624868+khrystuk@users.noreply.github.com>
2018-11-20 22:36:20 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Setting up..."
+
+xcode-select --install
+
+if test ! $(which brew); then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update && brew upgrade
+brew tap homebrew/bundle
+brew bundle
+
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
