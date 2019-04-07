8cecb0d6a85879b3bc06c44dcb2dab4c564c33fb
yura khrystuk <22624868+khrystuk@users.noreply.github.com>
2019-04-07 21:21:32 +0300

    added tools for csharp

--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ code --install-extension esbenp.prettier-vscode
 code --install-extension ms-python.python
 code --install-extension mikestead.dotenv
 code --install-extension aaron-bond.better-comments
+code --install-extension unity.unity-debug
+code --install-extension ms-vscode.csharp
 
 # NPM global modules
 npm i -g eslint nodemon prettier pure-prompt trash-cli typescript
