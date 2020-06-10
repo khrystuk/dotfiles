6753eddc057b0c14421161051dc5a0bb1d9a3e80
yurii khrystuk <22624868+khrystuk@users.noreply.github.com>
2020-06-10 19:54:27 +0300

    update

--- setup.sh
+++ setup.sh
@@ -22,6 +22,9 @@ pip3 install pipenv
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
 
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
