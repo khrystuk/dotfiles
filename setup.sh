32fc32ffcb1dbd772a0820eaebaabfe9a63a2a3f
Yurii Khrystuk <22624868+khrystuk@users.noreply.github.com>
2020-08-07 20:55:42 +0300

    update

--- setup.sh
+++ setup.sh
@@ -19,9 +19,6 @@ npm i -g eslint nodemon prettier pure-prompt trash-cli typescript
 pip3 install pipenv
 
 # MACOS SETTINGS
-# Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
-
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
